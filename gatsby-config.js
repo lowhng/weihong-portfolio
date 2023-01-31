d9881fe48aeb41c703e04fa24d79dab966e60191
wlo <weihong.edu@outlook.com>
2023-01-31 16:12:51 +1300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Wei Hong's Portfolio`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
